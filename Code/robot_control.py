4d7bff864f1a24d5f344ff644fa9ff3fd9c16749
Seba <seb.zapata8@gmail.com>
2022-12-05 11:18:12 -0700

    updatd dh parameters and joint limits

--- Code/robot_control.py
+++ Code/robot_control.py
@@ -3,17 +3,21 @@ from robot_arm import *
 
 VISUAL_SCALE_FACTOR = 10
 
-a_len = 0.14 * VISUAL_SCALE_FACTOR
-b_len = 0.02 * VISUAL_SCALE_FACTOR
-c_len = 0.07 * VISUAL_SCALE_FACTOR
+a_len = 0.1395 * VISUAL_SCALE_FACTOR
+b_len = 0.01995 * VISUAL_SCALE_FACTOR
+c_len = 0.06915 * VISUAL_SCALE_FACTOR
+d_len = 0.06925 * VISUAL_SCALE_FACTOR
+e_len = 0.1372 * VISUAL_SCALE_FACTOR
 dh_parameters = [[np.pi/2.0, a_len, b_len, np.pi/2.0],
                  [0, 0, c_len, 0],
-                 [0, 0, c_len, 0]]
+                 [0, 0, d_len, 0],
+                 [0, 0, e_len, 0]]
 
 # joints = ['r', 'p', 'r', 'p']
 
 joint_limits = [[-180, 180],
                 [-70, 90],
+                [-90, 90],
                 [-90, 90]]
 
 arm = RobotArm(dh_parameters, joint_limits=joint_limits)
