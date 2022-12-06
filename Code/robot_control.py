dd3a316b77ba8d33642c8e336a1235d2a8155b1a
Seba <seb.zapata8@gmail.com>
2022-12-06 11:58:08 -0700

    included constants for IK operation

--- Code/robot_control.py
+++ Code/robot_control.py
@@ -3,11 +3,16 @@ from robot_arm import *
 
 VISUAL_SCALE_FACTOR = 10
 
+IK_TARGET_POSITIONS = [[2, 0, 0.5]]
+OBSTACLES = [0.5, 0.75, 0.5]
+OBSTACLE_RADII = [0.2]
+
 a_len = 0.1395 * VISUAL_SCALE_FACTOR
 b_len = 0.01995 * VISUAL_SCALE_FACTOR
 c_len = 0.06915 * VISUAL_SCALE_FACTOR
 d_len = 0.06925 * VISUAL_SCALE_FACTOR
 e_len = 0.1372 * VISUAL_SCALE_FACTOR
+
 dh_parameters = [[np.pi/2.0, a_len, b_len, 3*np.pi/2.0],
                  [0, 0, c_len, 0],
                  [0, 0, d_len, 0],
@@ -21,4 +26,4 @@ joint_limits = [[-180, 180],
                 [-90, 90]]
 
 arm = RobotArm(dh_parameters, joint_limits=joint_limits)
-ArmPlayer(arm)
+ArmPlayer(arm, IK_TARGET_POSITIONS, OBSTACLES, OBSTACLE_RADII)
